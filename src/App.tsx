20e35f3b3bc05c3a24d16688383357d127486559
Daisuke Yamamura <mucho613@gmail.com>
2023-12-11 08:56:18 +0900

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,33 @@ function App() {
   const { loadFile, play, getPlayStatus } = useInvoke();
 
   return (
-    <div class="container">
+    <div class="container m-4">
       <div>
-        <input type="text" onInput={(e) => setFilePath(e.target.value)} />
-        <button type="button" onClick={() => loadFile(filePath())}>
-          Load file
-        </button>
-        <button type="button" onClick={play}>
-          Play
-        </button>
+        <input class="mb-1 w-full" type="text" onInput={(e) => setFilePath(e.target.value)} />
+        <div class="flex gap-1">
+          <div class="flex flex-col w-[66px] gap-1">
+            <button
+              class="pl-px text-left h-[17px] bg-blue leading-none"
+              type="button"
+              onClick={() => loadFile(filePath())}
+            >
+              LOAD
+            </button>
+            <button
+              class="pl-px text-left h-[17px] bg-blue leading-none"
+              type="button"
+              onClick={play}
+            >
+              PLAY
+            </button>
+          </div>
+          <div>
+            <h2>Track Monitor</h2>
+          </div>
+        </div>
       </div>
 
-      <TrackMonitor getPlayStatus={getPlayStatus} />
+      {/* <TrackMonitor getPlayStatus={getPlayStatus} /> */}
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      blue: '#2C4A7E'
+    }
+  },
+  plugins: [],
+}
+
