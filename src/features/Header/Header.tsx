37053348c068d82c577223fc5c4df8c4dbd154d1
Daisuke Yamamura <mucho613@gmail.com>
2024-12-30 03:11:20 +0900

    fix: header parts

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -15,12 +15,16 @@ function Header(props: Props) {
 
   return (
     <div className="flex items-start gap-x-[7px]">
-      <div className="flex gap-x-1.5 *:bg-sted-blue *:pl-2">
-        <div className="w-[113px] pr-[9px]">FREE: {freeMemory}</div>
-        <div className="w-[114px] pr-2.5">USED: {usedMemory}</div>
-        <div className="w-[66px] pr-2.5">TR.: {trackNumber}</div>
-        <div className="w-[178px] pr-2.5">MODULE: {moduleName}</div>
-      </div>
+      <dl className="flex *:bg-sted-blue">
+        <dt className="pl-2 after:content-[':']">FREE</dt>
+        <dd className="w-[65px] text-right pr-[9px] mr-1.5">{freeMemory}</dd>
+        <dt className="pl-[9px] after:content-[':']">USED</dt>
+        <dd className="w-[65px] text-right pr-[9px] mr-1.5">{usedMemory}</dd>
+        <dt className="pl-[9px] after:content-[':']">TR.</dt>
+        <dd className="w-[25px] text-right pr-[9px] mr-1.5">{trackNumber}</dd>
+        <dt className="pl-[9px] after:content-[':']">MODULE</dt>
+        <dd className="w-[113px] text-right pr-[9px]">{moduleName}</dd>
+      </dl>
 
       {/* Title area */}
       <div className="h-8 w-[272px] bg-sted-blue border border-sted-gray">
