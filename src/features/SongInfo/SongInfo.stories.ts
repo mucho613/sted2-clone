66c1572ad3b87dd44f019f13839d774739d221aa
Daisuke Yamamura <mucho613@gmail.com>
2025-01-10 23:52:06 +0900

    feat: SongInfo story

---
+++ src/features/SongInfo/SongInfo.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import SongInfo from "./SongInfo";
+
+const meta = {
+  component: SongInfo,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof SongInfo>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    tempo: 120,
+    timebase: 96,
+    playBias: 0,
+    keyName: "C MAJOR",
+    beat: "4 / 4",
+    midiInputDeviceName: "InDevice",
+  },
+};
